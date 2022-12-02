3b15fb1a169f3673cb0c57a5cef19663c75aacd5
khanhworktime <chikhanh5@gmail.com>
2022-12-03 00:57:10 +0700

    add new admin branch

--- client/src/interface/IDish.tsx
+++ client/src/interface/IDish.tsx
@@ -5,4 +5,32 @@ interface IDish{
     name: string;
 }
 
+export const fakeDishes:Array<IDish> = [{
+    id: "D01",
+    description: "ABX",
+    state: "available",
+    name: "Mon D01"
+},{
+    id: "D02",
+    description: "ABX",
+    state: "available",
+    name: "Mon D02"
+},{
+    id: "D03",
+    description: "ABX",
+    state: "available",
+    name: "Mon 03"
+},{
+    id: "D04",
+    description: "ABX",
+    state: "available",
+    name: "Mon 04"
+},{
+    id: "D05",
+    description: "ABX",
+    state: "available",
+    name: "Mon 05"
+}
+]
+
 export default  IDish;
